f3bb312d0aa196254292f902e446c4e1c1552f58
Skull <yourxsneha@gmail.com>
2025-03-12 21:44:39 +0530

    Update generate.py

--- plugins/generate.py
+++ plugins/generate.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 import traceback
 from pyrogram.types import Message
@@ -83,7 +80,3 @@ async def main(bot: Client, message: Message):
         return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
     await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
 
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
